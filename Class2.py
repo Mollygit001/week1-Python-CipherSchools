24619b5fbb3614da1060b0d095d3330d07478b22
Mollygit001 <119070721+Mollygit001@users.noreply.github.com>
2022-11-30 22:31:30 +0530

    Add files via upload
    Week1 Python Source code

---
+++ Class2.py
@@ -0,0 +1,43 @@
+#Hello World
+print("Hello")
+print("Hello World")
+
+#This is a comment
+
+'''
+Two Types of executable line:
+ 1)Statement
+ 2)Expression
+'''
+
+
+#Data Types
+print(isinstance("a",str))
+print(isinstance("a",int))
+print(isinstance(2,str))
+print(isinstance("a",object))
+print(isinstance(2,object))
+print(isinstance(print,object))
+
+a=255
+print(a)
+a="a"
+print(a)
+type(a)
+print(id(a))
+b=200
+print(id(b))
+c=278
+print(id(c))
+
+a="aaaaa"
+print(id(a))
+b="aaaaa"
+print(id(b))
+
+a=5677453456754535246681255632435678456789765463
+print(a+1)
+
+# variables
+_a=12
+a1=1
